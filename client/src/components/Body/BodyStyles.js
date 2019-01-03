17e1eba6238dcc5213bbca009d5416e6b00987c7
xezian <xezian@icloud.com>
2019-01-02 22:22:32 -0700

    body height fix using viewportheight

--- client/src/components/Body/BodyStyles.js
+++ client/src/components/Body/BodyStyles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const BodyStyles = styled.div`
   background-color: ${props => props.theme.mainBackground};
   color: ${props => props.theme.textOne};
-  height: 100%;
+  min-height: 100vh;
   padding: 15vh 0;
 `;
 
